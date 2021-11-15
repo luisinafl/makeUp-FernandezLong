82f1959a94766146e34ec86f21362b7cf0ec144f
Luisina Fernández Long <luisina.fl@hotmail.com>
2021-11-14 21:21:32 -0300

    Agrego clase Foundations y defino 2

--- index.js
+++ index.js
@@ -2,12 +2,29 @@ var Client = /** @class */ (function () {
     function Client(name, isClient) {
         this.name = name;
         this.isClient = isClient;
-        this.name = name,
-            this.isClient = isClient;
+        this.name = name;
+        this.isClient = isClient;
     }
     return Client;
 }());
-var maria = new Client('maria', true);
-var lucia = new Client('lucia', false);
+var maria = new Client("maria", true);
+var lucia = new Client("lucia", false);
 var clients = [maria, lucia];
 console.log(clients);
+var Foundation = /** @class */ (function () {
+    function Foundation(brand, shade, coverage, price) {
+        this.brand = brand;
+        this.shade = shade;
+        this.coverage = coverage;
+        this.price = price;
+        this.brand = brand;
+        this.shade = shade;
+        this.coverage = coverage;
+        this.price = price;
+    }
+    return Foundation;
+}());
+var fitMe = new Foundation('Maybelline FitMe', 280, 'full', 23.95);
+var infallible = new Foundation('Loreal Infallible', 'Golden beige', 'medium', 54);
+var foundations = [fitMe, infallible];
+console.log(foundations);
--- index.ts
+++ index.ts
@@ -1,18 +1,37 @@
-
 class Client {
     constructor(
-
         public name: string,
         public isClient: boolean
-    ){
-        this.name = name,
+    ) {
+        this.name = name
         this.isClient = isClient
     }
 }
 
-let maria = new Client('maria', true);
-let lucia = new Client('lucia', false)
+let maria = new Client("maria", true);
+let lucia = new Client("lucia", false);
 
+let clients = [maria, lucia];
+console.log(clients);
 
-let clients = [maria, lucia]
-console.log(clients)
+class Foundation {
+
+    constructor(
+        public brand: string,
+        public shade: string | number,
+        public coverage: string,
+        public price: number
+    ) {
+        this.brand = brand
+        this.shade = shade
+        this.coverage = coverage
+        this.price = price
+    }
+
+}
+
+let fitMe = new Foundation('Maybelline FitMe', 280, 'full', 23.95);
+let infallible = new Foundation('Loreal Infallible', 'Golden beige', 'medium', 54)
+
+let foundations = [fitMe, infallible];
+console.log(foundations);
