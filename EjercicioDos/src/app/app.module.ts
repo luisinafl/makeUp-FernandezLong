1f8e7ec0895e3d595ba827213964a300e79be76a
Luisina Fernández Long <luisina.fl@hotmail.com>
2021-11-22 18:52:11 -0300

    Creo un componente y verfigico el funcionamiento de BS

--- EjercicioDos/src/app/app.module.ts
+++ EjercicioDos/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
+import { CardModule } from './card/card.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
